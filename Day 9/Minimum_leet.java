da1a135f41bd34a3e6ea6fe19e34eb5441b96558
Kumar Dev <91386391+Dev2409@users.noreply.github.com>
2024-02-04 21:27:29 +0530

    Create Minimum_leet.java

---
+++ Day 9/Minimum_leet.java
@@ -0,0 +1,35 @@
+class Minimum_leet {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
+        }
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+        /// UPVOTE !
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+
+        char[] chS = s.toCharArray();
+
+        while (end < chS.length) {
+            if (map[chS[end++]]-- > 0) {
+                count--;
+            }
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
+                }
+                if (map[chS[start++]]++ == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? new String() :
+                new String(chS, startIndex, minLen);
+    }
+}
