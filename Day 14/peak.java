c12396e4901f1804b9b9fa7c8370204e9c890e71
Kumar Dev <91386391+Dev2409@users.noreply.github.com>
2024-02-09 21:22:03 +0530

    Create peak.java

---
+++ Day 14/peak.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class peak {
+    public static int peak(int [] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int result=peak(arr);
+        System.out.println(arr[result]);
+
+
+
+    }
+}
